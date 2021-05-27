1c7a07c1c48e7e7b17561d9ffa6cbf5b111b966d
nagarajankola <ankola.nagraj07@gmail.com>
2021-05-27 16:41:21 +0530

    ..

--- client/src/components/ArticleList.jsx
+++ client/src/components/ArticleList.jsx
@@ -15,7 +15,7 @@ export default function ArticleList() {
 
   useEffect(async () => {
     // console.log(articleList);
-    const res = await fetch("/allarticles", {
+    const res = await fetch("https://iblog-nagraj.herokuapp.com/allarticles", {
       method: "GET",
       mode: "no-cors"
     })
